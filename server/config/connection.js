eacb7e00c9175ef2e5d04026369b786d0085ce70
Michael Mattingly <mikeamatt1@hotmail.com>
2024-03-24 22:16:04 -0400

    updated mongodburi for production

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,5 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
 
-const dev = process.env.MODE;
-
-if (!dev) {
-    mongoose.connect(process.env.MONGODB_URI)
-    console.log("Connected to production database.")
-} else {
-    mongoose.connect("mongodb://127.0.0.1:27017/date-planner")
-    console.log("Connected to development database.")
-}
+mongoose.connect(process.env.MONGODB_URI)
 
 module.exports = mongoose.connection;
