0fa68b7b2d62966410ca15245612102e4d7cb44c
Michael Mattingly <mikeamatt1@hotmail.com>
2024-03-24 22:50:15 -0400

    updated production build

--- server/server.js
+++ server/server.js
@@ -5,23 +5,26 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const startServer = async () => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-app.use(require("./controllers"));
+  app.use(require("./controllers"));
 
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    });
+  }
 
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`API URL: http://localhost:${PORT}/api`);
+    });
   });
+};
 
-
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`)
-    console.log(`API URL: http://localhost:${PORT}/api`);
-  });
-});
+startServer();
