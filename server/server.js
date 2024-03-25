af26b4538a5cfafc7f667975ec27a7f74ba202ed
Michael Mattingly <mikeamatt1@hotmail.com>
2024-03-24 22:39:58 -0400

    updated for production

--- server/server.js
+++ server/server.js
@@ -11,13 +11,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(require("./controllers"));
 
 
-if (process.env.NODE_ENV === "production") {
+
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
-}
+
 
 db.once("open", () => {
   app.listen(PORT, () => {
