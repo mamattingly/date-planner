ea8a82a8e623e09196c10b1dcface05302e0f82d
Michael Mattingly <mikeamatt1@hotmail.com>
2024-03-25 21:29:35 -0400

    updated routes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 
-const router = require('./controllers/index.js');
+const router = require("./controllers/index.js");
 
 dotenv.config();
 
@@ -14,13 +14,18 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
 app.use(router);
 
-app.use('/', express.static(path.join(__dirname, '/client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+  });
+}
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
   console.log(`starting on port ${PORT}`);
   app.listen(PORT);
-})
+});
