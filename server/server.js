9c1789024fd3bbf991bf51b2c3b79c6d46280541
Michael Mattingly <mikeamatt1@hotmail.com>
2024-03-25 21:38:01 -0400

    updated path for dist

--- server/server.js
+++ server/server.js
@@ -20,9 +20,9 @@ app.use(morgan("tiny"));
 app.use(router);
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "../../client/build")));
+  app.use("/", express.static(path.join(__dirname, "../client/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
   });
 }
 
